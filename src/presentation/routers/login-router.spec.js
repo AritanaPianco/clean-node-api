b9b6cd2d9af3ceef07624856d9a98e0915697fc2
AritanaPianco <aritana.pianco@gmail.com>
2024-12-07 19:11:22 -0300

    ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -102,7 +102,7 @@ describe('Login Router', () => {
     expect(httpResponse.statusCode).toBe(500)
   })
   test('should return 200 when valid credentials is provided', () => {
-    const { sut } = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
     const httpRequest = {
       body: {
         email: 'valid_email@email.com',
@@ -111,5 +111,6 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken)
   })
 })
